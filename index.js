35fd7d0933676af44870e376723430907ffce6e7
Peter <pschoenbauer@gmail.com>
2023-03-18 22:22:20 -0400

    more of dem functions lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun (activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun("bathe my dog")
+
+function mondayWork (work = "go to the office"){
+    return `This Monday, I will ${work}.`
+}
+mondayWork("work from home")
+
+function wrapAdjective(character, start = 'You are') {
+    return function (end = "special") {
+      return `${start} ${character}${end}${character}!`;
+    };
+  }
